c9c82efae044344a1e05dcf69e64729b10ab7c6a
ericvinicius <eric.camargo@gmail.com>
2018-01-10 00:36:48 -0200

    WIP

--- src/main/java/br/com/eric/telegram/kerobot/models/MessageResponse.java
+++ src/main/java/br/com/eric/telegram/kerobot/models/MessageResponse.java
@@ -1,5 +1,62 @@
 package br.com.eric.telegram.kerobot.models;
 
 public class MessageResponse {
+	private boolean ok;
+	private Result result;
+
+	public class Result {
+		private User from;
+		private Chat chat;
+		private Integer date;
+		private String text;
+
+		public User getFrom() {
+			return from;
+		}
+
+		public void setFrom(User from) {
+			this.from = from;
+		}
+
+		public Chat getChat() {
+			return chat;
+		}
+
+		public void setChat(Chat chat) {
+			this.chat = chat;
+		}
+
+		public Integer getDate() {
+			return date;
+		}
+
+		public void setDate(Integer date) {
+			this.date = date;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+	}
+
+	public boolean isOk() {
+		return ok;
+	}
+
+	public void setOk(boolean ok) {
+		this.ok = ok;
+	}
+
+	public Result getResult() {
+		return result;
+	}
+
+	public void setResult(Result result) {
+		this.result = result;
+	}
 
 }
