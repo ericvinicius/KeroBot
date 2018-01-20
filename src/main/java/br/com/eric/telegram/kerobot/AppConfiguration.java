0b7fdff3bb51f15d1ec8d788c9b5849b0b9f6f10
ericvinicius <eric.camargo@gmail.com>
2018-01-20 00:00:39 -0200

    Redirect Http to Https

--- src/main/java/br/com/eric/telegram/kerobot/AppConfiguration.java
+++ src/main/java/br/com/eric/telegram/kerobot/AppConfiguration.java
@@ -2,7 +2,12 @@ package br.com.eric.telegram.kerobot;
 
 import java.util.Collections;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,5 +44,31 @@ public class AppConfiguration {
 		registrationBean.setFilter(new ShutdownFilter());
 		registrationBean.setUrlPatterns(Collections.singleton("/shutdown"));
 		return registrationBean;
+	}
+
+	@Bean
+	public TomcatEmbeddedServletContainerFactory tomcat() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+		return tomcat;
+	}
+
+	private Connector createHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setSecure(false);
+		connector.setPort(8080);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 }
