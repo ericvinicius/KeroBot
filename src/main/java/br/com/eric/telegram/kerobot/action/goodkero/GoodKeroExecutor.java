f313ab0b25fb7c05d274c97d36a82275f16eb381
ericvinicius <eric.camargo@gmail.com>
2018-01-20 10:39:01 -0200

    Simple fix

--- src/main/java/br/com/eric/telegram/kerobot/action/goodkero/GoodKeroExecutor.java
+++ src/main/java/br/com/eric/telegram/kerobot/action/goodkero/GoodKeroExecutor.java
@@ -19,7 +19,7 @@ public class GoodKeroExecutor {
 	@Autowired
 	private GiphyApi giphyApi;
 	
-	private static int nightGoodTime = 0;
+	private static int NIGHT_TRIES = 0;
 
 	public void compliment(Update update) {
 		Response giphy = giphyApi.random(GiphyApi.Rating.G.getName(), "thanks", GiphyApi.TOKEN);
@@ -45,17 +45,18 @@ public class GoodKeroExecutor {
 			doIt(chatId, time);
 			return;
 		} else if (timeOfDay == 24 || timeOfDay < 6) {
-			if (nightGoodTime > 3) {
-				nightGoodTime = 0;
+			if (NIGHT_TRIES >= 3) {
+				NIGHT_TRIES = 0;
 				botApi.sendMessage(chatId, "Estou dormindo agora... ");
 			} else {
-				nightGoodTime++;
+				NIGHT_TRIES++;
 				String txt = "";
-				for (int i = 0; i < nightGoodTime; i++) {
+				for (int i = 0; i < NIGHT_TRIES; i++) {
 					txt += ":sleepy:";
 				}
 				botApi.sendMessage(chatId, txt);
 			}
+			return;
 		}
 		failDoIt(chatId, time);
 
