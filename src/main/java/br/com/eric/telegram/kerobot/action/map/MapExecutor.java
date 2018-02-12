716add3e761de3b150daaac5959356ddcf44705f
ericvinicius <eric.camargo@gmail.com>
2018-02-12 00:03:33 -0200

    Melhorando o texto das chaves

--- src/main/java/br/com/eric/telegram/kerobot/action/map/MapExecutor.java
+++ src/main/java/br/com/eric/telegram/kerobot/action/map/MapExecutor.java
@@ -25,24 +25,26 @@ public class MapExecutor {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	private static final Logger logger = LogManager.getLogger(MapExecutor.class);
 
 	public void get(MessageModel message, String key, String customUsername) {
-		String username = (customUsername != null && !customUsername.isEmpty()) ? customUsername : message.getFrom().getUsername();
+		String username = (customUsername != null && !customUsername.isEmpty()) ? customUsername
+				: message.getFrom().getUsername();
 		logger.info("Get key: " + key + " for user: " + username);
 		Optional<UserModel> userOp = userRepository.findOneByUsername(username.replaceAll("@", ""));
 		if (userOp.isPresent()) {
 			UserModel user = userOp.get();
 			Optional<MapModel> keyValueOp = keyValueRepository.findByUserIdAndChave(user.getId(), key);
 			if (keyValueOp.isPresent()) {
-				botApi.sendMessage(message.getChat().getId(), "Chave: " + key + "\nValor: " + keyValueOp.get().getValor());
+				sendKey(message.getChat().getId(), user.getUsername(), key, keyValueOp.get().getValor(), null);
 				return;
 			}
-			botApi.sendMessage(message.getChat().getId(), "Usuario @" + username + " nao possui nada salvo para a chave: " + key);
+			botApi.sendMessage(message.getChat().getId(),
+					"Usuario @" + username + " nao possui nada salvo para a chave: " + key);
 			return;
 		}
-		botApi.sendMessage(message.getChat().getId(), "Usuario @" + username + " nao encontrado!");		
+		botApi.sendMessage(message.getChat().getId(), "Usuario @" + username + " nao encontrado!");
 	}
 
 	public void edit(MessageModel message, String key, String value, String customUser) {
@@ -56,17 +58,28 @@ public class MapExecutor {
 				MapModel mapModel = keyValueOp.get();
 				String previous = mapModel.getValor();
 				keyValueRepository.save(mapModel.setValor(value));
-				botApi.sendMessage(message.getChat().getId(), "Chave: " + key + "\nValor: " + value + "\nAnterior: " + previous);
+				sendKey(message.getChat().getId(), user.getUsername(), key, value, previous);
 				return;
 			} else {
 				MapModel keyValue = new MapModel(user.getId(), key, value);
 				keyValueRepository.save(keyValue);
-				botApi.sendMessage(message.getChat().getId(), "Chave: " + key + "\nValor: " + value);
+				sendKey(message.getChat().getId(), user.getUsername(), key, value, null);
 				return;
 			}
 		}
 		botApi.sendMessage(message.getChat().getId(), "Usuario @" + username + " nao encontrado!");
 
+	}
+
+	private void sendKey(Integer id, String username, String key, String value, String previous) {
+		StringBuilder stringBuilder = new StringBuilder().append("Usuario: @").append(username).append("\nChave: ")
+				.append(key).append("\nValor: ").append(value);
+		
+		if (previous != null && !previous.isEmpty()) {
+			stringBuilder.append("\nAnterior: ").append(previous);
+		}
+		
+		botApi.sendMessage(id, stringBuilder.toString());
 	}
 
 	public void list(MessageModel message) {
